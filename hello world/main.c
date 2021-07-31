00cfc9329d2199f280f72844123fc31cda571499
DejavuMoe <jialong.vip@gmail.com>
2021-07-31 09:51:04 +0800

    :sparkles: first commit

---
+++ hello world/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>                  // 预处理指令
+int main()                         // int main()是主函数，程序从这开始执行
+{
+    printf("Hello, World!\n");     // printf()函数，打印输出"Hello,Wolrd!"
+    return 0;                      // 终止main()函数，并返回0
+}
+/* 第一个C程序："Hello World!" */      // 由"/*...*/"包裹起来的这部分是注释内容
